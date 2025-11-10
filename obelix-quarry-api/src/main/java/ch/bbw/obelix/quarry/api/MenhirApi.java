743c0ea46ea2012d77a4490617bb96914575f9ef
Andrin <a.brit@icloud.com>
2025-11-10 09:24:27 +0100

    API für einzelne Menhir-Operationen erweitert
    - Neue Methode getMenhirById zum Abrufen eines einzelnen Menhirs
    - Neue Methode deleteById zum Löschen eines Menhirs
    - Nötige Import-Anweisungen hinzugefügt
    
    Diese Änderung ist der erste Schritt, um später den Webshop in
    kleinere Services aufzuteilen. Die bestehenden Endpunkte bleiben
    dabei unverändert.

--- obelix-quarry-api/src/main/java/ch/bbw/obelix/quarry/api/MenhirApi.java
+++ obelix-quarry-api/src/main/java/ch/bbw/obelix/quarry/api/MenhirApi.java
@@ -1,10 +1,19 @@
 package ch.bbw.obelix.quarry.api;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface MenhirApi {
     @GetExchange("/api/menhirs")
     List<MenhirDto> getAllMenhirs();
+
+    @GetExchange("/api/menhirs/{menhirId}")
+    MenhirDto getMenhirById(@PathVariable("menhirId") UUID menhirId);
+
+    @DeleteExchange("/api/quarry/{menhirId}")
+    void deleteById(@PathVariable("menhirId") UUID menhirId);
 }
